c9ec3db17b6a0d3612ad96a2f254b4a53010c55a
dafa779 <bene567899@gmail.com>
2026-04-03 08:21:43 +0700

    Update translator.py

--- translator.py
+++ translator.py
@@ -1,4 +1,16 @@
-from deep_translator import GoogleTranslator
+from googletrans import Translator
+
+translator = Translator()
 
 def translate(text):
-    return GoogleTranslator(source='auto', target='vi').translate(text)
+
+    detect = translator.detect(text).lang
+
+    if detect == "zh-cn" or detect == "zh-tw":
+        result = translator.translate(text, src="zh-cn", dest="vi")
+    elif detect == "vi":
+        result = translator.translate(text, src="vi", dest="zh-cn")
+    else:
+        result = translator.translate(text, dest="vi")
+
+    return result.text
