64a86c68f91354f46a77365367db6b2f88fa0e71
dafa779 <bene567899@gmail.com>
2026-04-03 08:29:01 +0700

    Refactor translate function to accept target language

--- translator.py
+++ translator.py
@@ -2,15 +2,6 @@ from googletrans import Translator
 
 translator = Translator()
 
-def translate(text):
-
-    detect = translator.detect(text).lang
-
-    if detect == "zh-cn" or detect == "zh-tw":
-        result = translator.translate(text, src="zh-cn", dest="vi")
-    elif detect == "vi":
-        result = translator.translate(text, src="vi", dest="zh-cn")
-    else:
-        result = translator.translate(text, dest="vi")
-
+def translate(text, target):
+    result = translator.translate(text, dest=target)
     return result.text
